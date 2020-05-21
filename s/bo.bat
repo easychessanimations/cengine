2fffa2f02bb1ceeec40cc32e3628650a7ae6dc0b
easychessanimations <chessbotroulette@gmail.com>
2020-05-21 15:46:55 +0200

    Favicon

--- s/bo.bat
+++ s/bo.bat
@@ -2,6 +2,7 @@ echo off
 rem call emcc src/cengine.cpp src/piece.cpp src/square.cpp src/bitboard.cpp src/attack.cpp src/state.cpp -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -D WASM -O2 -o wasmbuild/cengine.html -s EXPORTED_FUNCTIONS="['_main','_execute_uci_command']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap']"
 copy srcweb\index.html site\index.html
 copy srcweb\index.js site\index.js
+copy srcweb\favicon.ico site
 type wasmbuild\cengine.js srcweb\preamble.js >site\cengine.js
 copy wasmbuild\cengine.wasm site
 copy wasmbuild\cengine.worker.js site
