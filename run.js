685b432fc194e67b8276067cdab827f99b3cf559
easychessanimations <chessbotroulette@gmail.com>
2020-06-04 16:48:36 +0200

    Memory access

--- run.js
+++ run.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
 const buf = fs.readFileSync('./simple.wasm');
 
-WebAssembly.instantiate(new Uint8Array(buf)).then(res => {
+const memory = new WebAssembly.Memory({initial:1})
+
+const i32 = new Uint32Array(memory.buffer)
+
+const importObject = { js: { mem: memory } }
+
+WebAssembly.instantiate(new Uint8Array(buf), importObject).then(res => {
 	const moduleExports = res.instance.exports
 
 	const addTwo = moduleExports.addTwo
 
-	console.log("addTwo(1,1) = ", addTwo(1,1))
+	i32[0] = 123
+
+	console.log("addTwo(1,1) = ", addTwo(0,1))
+
+	i32[0] = 456
+
+	console.log("addTwo(1,1) = ", addTwo(0,1))
 })
--- simple.wat
+++ simple.wat
@@ -1,7 +1,9 @@
 (module
-  (func (export "addTwo") (param i32 i32) (result i32)
-    local.get 0
-    local.get 1
-    i32.add
-  )
+	(import "js" "mem" (memory 1))
+	(func (export "addTwo") (param i32 i32) (result i32)						
+		get_local 0
+		i32.load
+		get_local 1
+		i32.add
+	)
 )
